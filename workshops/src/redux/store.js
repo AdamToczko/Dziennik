07c48257ca52dd2707460a8c5d28a82ad11aa072
Kacper Grzeszczyk <kacgrzes@gmail.com>
2019-11-26 12:26:30 +0100

    Connect apple module to store

--- workshops/src/redux/store.js
+++ workshops/src/redux/store.js
@@ -2,10 +2,19 @@ import { createStore, combineReducers } from "redux";
 import { devToolsEnhancer } from "redux-devtools-extension";
 import counterReducer, { increment, add, subtract } from "./counter";
 import todosReducer from "./todos";
+import appleReducer, {
+  addWorm,
+  bite,
+  clean,
+  grow,
+  removeWorm,
+  rot
+} from "./apple";
 
 const rootReducer = combineReducers({
   todos: todosReducer,
-  counter: counterReducer
+  counter: counterReducer,
+  apple: appleReducer
 });
 
 const store = createStore(rootReducer, devToolsEnhancer());
@@ -19,3 +28,17 @@ window.clickIncrement = () => dispatch(increment());
 
 // action creator -> action -> dispatch
 dispatch(subtract(10));
+
+const actions = [
+  grow(),
+  addWorm(),
+  clean(),
+  removeWorm(),
+  bite(),
+  bite(),
+  bite(5),
+  bite(10),
+  rot()
+];
+
+actions.forEach(dispatch);
