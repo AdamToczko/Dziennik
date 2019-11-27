e823a276065663f4163a64394db57c8476ff3802
Kacper Grzeszczyk <kacgrzes@gmail.com>
2019-11-27 12:57:45 +0100

    Add props that control how Apple looks like

--- workshops/src/components/Apple.js
+++ workshops/src/components/Apple.js
@@ -1,14 +1,43 @@
 import React from "react";
 import styles from "./Apple.module.css";
 
-const Apple = () => {
+const Apple = props => {
   return (
     <div className={styles.container}>
-      <div className={styles.worm}>:)</div>
-      <div className={styles.appleTail}></div>
-      <div className={styles.apple}></div>
+      {props.hasWorm && (
+        <div onClick={props.onRemoveWorm} className={styles.worm}>
+          :)
+        </div>
+      )}
+      {props.bites === 10 && <div className={styles.appleTail}></div>}
+      <div
+        onClick={props.onAppleClick}
+        className={styles.apple}
+        style={{
+          height: props.bites * 20 + "px",
+          width: `${props.bites * 20}px`,
+          backgroundColor: props.isDirty ? "grey" : props.color,
+          borderColor: props.isDirty ? "grey" : props.color
+        }}
+      ></div>
+      {!props.isEatable && (
+        <div className={styles.warning}>
+          <p>
+            Apple is not eatable. Please remove worm, clean and make sure apple
+            exists. Remember: apple should be red!!!
+          </p>
+        </div>
+      )}
     </div>
   );
+};
+
+Apple.defaultProps = {
+  hasWorm: false,
+  color: "green",
+  bites: 1,
+  isDirty: true,
+  isEatable: false
 };
 
 export default Apple;
