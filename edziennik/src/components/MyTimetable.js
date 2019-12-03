786a68af30dda1b9344ac64ef7bfce6f4053e180
Adam Toczko <adamtoczko80@gmail.com>
2019-12-03 12:51:25 +0100

    MyTimetable logic

--- edziennik/src/components/MyTimetable.js
+++ edziennik/src/components/MyTimetable.js
@@ -1,5 +1,16 @@
 import React from 'react'
-import { Subtitle, Columns, Column } from 'bloomer'
+import { Subtitle, Button } from 'bloomer'
+import textToColor from '../helpers/textToColor'
+
+const daysSchedules = [
+  [{hours: 2, text: 'JS'}, {hours: 1, text: 'HTML'}, {hours: 5, text: 'CSS'}],
+  [{hours: 5, text: 'JS'}, {hours: 2, text: 'HTML'}, {hours: 1, text: 'CSS'}],
+  [{hours: 2, text: 'JS'}, {hours: 3, text: 'HTML'}, {hours: 3, text: 'CSS'}],
+  [{hours: 2, text: 'React'}, {hours: 1, text: 'HTML'}, {hours: 1, text: 'CSS'}],
+  [{hours: 2, text: 'HTML'}, {hours: 6, text: 'CSS'}],
+  [],
+  []
+]
 
 const ONE_HOUR_IN_PIXELS = 80
 
@@ -31,25 +42,39 @@ const Days = () => {
 }
 
 const DaySchedule = props => {
+  const {daySchedules} = props
+  const scheduledHours = daySchedules.reduce((acc, daySchedule) => {
+    return acc + daySchedule.hours
+  }, 0)
+
   const oneDayInPixels = 8 * ONE_HOUR_IN_PIXELS + 'px' // 480px
-  return <div style={{ flex: 1, height: oneDayInPixels, background: 'yellow' }}>
-    <DayScheduleItem hours={1}/>
-    <DayScheduleItem hours={2}/>
-    <DayScheduleItem hours={1}/>
-    <DayScheduleItem hours={4}/>
+  return <div style={{ flex: 1, height: oneDayInPixels, background: 'white' }}>
+    {daySchedules.map((daySchedule, index) => {
+      return <DayScheduleItem key={index} text={daySchedule.text} hours={daySchedule.hours} />
+    })}
+    {scheduledHours < 8 ? <Button isFullWidth isColor="info">+</Button> : null}
   </div>
 }
   
 const DayScheduleItem = props => {
+  const {text = '', hours = 2} = props
+  const color = textToColor(text)
+  console.log(color)
+  
   return <div style={{
     width: '100%',
-    height: ONE_HOUR_IN_PIXELS * props.hours + 'px',
-    background: 'lightgrey',
+    height: ONE_HOUR_IN_PIXELS * hours + 'px',
+    background: color,
     boxSizing: 'border-box',
     border: '2px solid black',
-    borderCollapse: 'collapse'
+    borderCollapse: 'collapse',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center'
   }}>
-    JS
+    <div style={{ padding: '8px', background: "white", color: "black", display: "inline", margin: '16px', borderRadius: "8px"}}>
+      {text}
+    </div>
   </div>
 }
 
@@ -57,13 +82,9 @@ const MyTimetable = props => {
   return <div>
     <Days />
     <div style={{display: 'flex', border: '2px solid black', boxSizing: 'border-box'}}>
-      <DaySchedule/>
-      <DaySchedule/>
-      <DaySchedule/>
-      <DaySchedule/>
-      <DaySchedule/>
-      <DaySchedule/>
-      <DaySchedule/>
+      {daysSchedules.map((daySchedules, index) => {
+        return <DaySchedule key={index} daySchedules={daySchedules} />
+      })}
     </div>
   </div>
 }
