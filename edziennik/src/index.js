b1453a7cef47d32acda458f2e5433192f61e1a10
Adam Toczko <adamtoczko80@gmail.com>
2019-12-03 09:55:52 +0100

    index js corrections

--- edziennik/src/index.js
+++ edziennik/src/index.js
@@ -7,10 +7,6 @@ import * as ClassService from "./services/ClassService"
 
 // fetchSkills()
 login("student10@example.com", "abc123")
-<<<<<<< HEAD
-addSkill('JS')
-=======
 // addSkill('JS')
 // ClassService.addClass("JFDD13")
-ClassService.addTeacherToClass("", "-Lv5fUxFOPEJWFE4sSrK")
->>>>>>> 6c83aa503a5b674d87643387975fe6fa34058b47
+ClassService.addTeacherToClass("", "46hVXHrQF7WAWM4Zi5yC5GzqqYP2")
